5c921acc01b5acf3a11934ce16e3b6c416c5ceac
ParceledTongue <zach@palumbo.io>
2017-03-30 11:42:12 -0400

    Add handling to avoid posting duplicate tweets
    Signed-off-by: ParceledTongue <zach@palumbo.io>

--- legally-bot.py
+++ legally-bot.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import tweepy, time, sys, markovify, random
+from sets import Set
 
-#enter the corresponding information from your Twitter application:
+# login via tweepy 
 CONSUMER_KEY = '***REMOVED***'
 CONSUMER_SECRET = '***REMOVED***'
 ACCESS_KEY = '***REMOVED***'
@@ -12,6 +13,12 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# build set of old tweets
+old_tweets = set()                                                          
+for tweet in tweepy.Cursor(api.user_timeline).items():
+  old_tweets.add(tweet.text)
+
+# generate models
 with open("lyrics.txt") as f:
   lyrics = f.read()
 
@@ -20,10 +27,13 @@ model_three = markovify.NewlineText(lyrics, state_size=3)
 model_four = markovify.NewlineText(lyrics, state_size = 4)
 models = [model_two, model_three, model_four]
 
+# main loop
 while True:
   tweet = None
-  while tweet == None:
+  while tweet == None or tweet in old_tweets:
     tweet = random.choice(models).make_short_sentence(140)
-  print(tweet)
   api.update_status(tweet)
+  print(tweet)
+  old_tweets.add(tweet)
   time.sleep(60 * 15)
+
