fc25da3635a71f91d5fc154c1f27ec3cb2e65aa3
Zach Palumbo <zach@palumbo.io>
2017-11-08 03:36:58 -0500

    No more caching or continous main loop
    Aaaaaand there goes most of the work that went into this...
    Caching was a cool idea, as was the implementation of the continuous
    main loop. But this is going to be hosted on Heroku, where a sleeping
    app still counts as active and will eat up my hours, so I need to use a
    scheduler there instead. The cache wouldn't have persisted betwen dyno
    executions, anyway.
    
    Signed-off-by: Zach Palumbo <zach@palumbo.io>

--- src/top40bot.py
+++ src/top40bot.py
@@ -1,59 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import datetime as dt, os, time, tweepy, util
-from history import History
+import os, tweepy, util
 
-HISTORY_SIZE = 1500
-TWEET_PERIOD = dt.timedelta(minutes = 30)
+HISTORY_SIZE = 1000
 
-# login via tweepy
-print('Logging in via tweepy')
-auth = tweepy.OAuthHandler(
-        os.environ['TWEEPY_CONSUMER_KEY'], 
-        os.environ['TWEEPY_CONSUMER_SECRET'])
-auth.set_access_token(
-        os.environ['TWEEPY_ACCESS_KEY'], 
-        os.environ['TWEEPY_ACCESS_SECRET'])
-api = tweepy.API(auth)
+def tweepy_api():
+    print('Logging in via tweepy')
+    auth = tweepy.OAuthHandler(
+            os.environ['TWEEPY_CONSUMER_KEY'], 
+            os.environ['TWEEPY_CONSUMER_SECRET'])
+    auth.set_access_token(
+            os.environ['TWEEPY_ACCESS_KEY'],
+            os.environ['TWEEPY_ACCESS_SECRET'])
+    return tweepy.API(auth)
 
-# build history of most recent HISTORY_SIZE tweets
-print('Building history of most recent ' + str(HISTORY_SIZE) + ' tweets')
-history = History(HISTORY_SIZE)
-old_tweets = []
-for tweet in tweepy.Cursor(api.user_timeline).items(HISTORY_SIZE):
-    old_tweets.append(tweet.text)
-for tweet in reversed(old_tweets):
-    history.add(tweet)
+def recent_tweets(num, tweepy_api):
+    print('Getting most recent ' + str(num) + ' tweets')
+    history = set()
+    for tweet in tweepy.Cursor(tweepy_api.user_timeline).items(num):
+        history.add(tweet.text)
+    return history
 
-# calculate when to make the first post
-next_post_time = dt.datetime.combine(
-        dt.date.today(), dt.time()) # midnight on the current day
-while next_post_time <= dt.datetime.now():
-    next_post_time += TWEET_PERIOD
-
-# we'll always start the main loop by updating the cache
-last_cache_update_date = dt.date.min
-
-# main loop
-while True:
-    # update the cache if we haven't today
-    if dt.date.today() > last_cache_update_date:
-       util.update_cache()
-       last_cache_update_date = dt.date.today()
-    # prepare the tweet
+if __name__ == "__main__":
+    tweepy_api = tweepy_api()
+    history = recent_tweets(HISTORY_SIZE, tweepy_api)
     print('Composing tweet')
     tweet = None
-    while tweet == None or tweet in history:
-        tweet = util.make_tweet()
-    print('Queued tweet is: ' + tweet)
-    # wait until the scheduled time to post
-    delta = (next_post_time - dt.datetime.now()).total_seconds()
-    print('Waiting until ' + str(next_post_time))
-    time.sleep(delta)
-    next_post_time += TWEET_PERIOD
-    # post the tweet
-    api.update_status(tweet)
+    while not tweet or tweet in history:
+        tweet = util.make_lyric()
     print('Tweeting: ' + tweet)
-    history.add(tweet)
+    tweepy_api.update_status(tweet)
 
--- src/util.py
+++ src/util.py
@@ -1,64 +1,17 @@
-import markovify, os, pickle, random
+import markovify, os, random
 from musixmatch import Musixmatch
 from PyLyrics import PyLyrics
 from track import Track
 
-CACHE_PATH = '../cache/cache.pkl'
-
 # set up Musixmatch API
 musixmatch = Musixmatch('***REMOVED***')
 
-def create_cache_dir_if_not_exists():
-    cache_dir = os.path.dirname(CACHE_PATH)
-    try:
-        os.makedirs(cache_dir)
-    except OSError:
-        if not os.path.isdir(cache_dir):
-            raise
+def top_40_tracks():
+    info_list = musixmatch.chart_tracks_get(1, 40, True) \
+            ['message']['body']['track_list']
+    return [Track(info) for info in info_list]
 
-def read_cache():
-    create_cache_dir_if_not_exists()
-    if os.path.isfile(CACHE_PATH):
-        with open(CACHE_PATH, 'rb') as f:
-            return pickle.load(f)
-    else:
-        return {}
-
-def write_cache(cache):
-    create_cache_dir_if_not_exists()
-    with open(CACHE_PATH, 'wb') as f:
-        pickle.dump(cache, f, pickle.HIGHEST_PROTOCOL)
-
-def update_cache():
-    cache = read_cache()
-    cache_tracks = set(cache.keys())
-    top40_tracks = set(get_top_40())
-
-    print('Updating cache...')
-    
-    if cache_tracks == top40_tracks:
-        print('  Already up to date.')
-        return cache
-
-    # add missing track info
-    new_tracks = top40_tracks - cache_tracks
-    for track in new_tracks:
-        print('  Adding ' + str(track) + ' to cache')
-        cache[track] = track.get_lyrics()
-  
-    # delete info for tracks no longer in the top 40
-    bumped_tracks = cache_tracks - top40_tracks
-    for track in bumped_tracks:
-        print('  Deleting ' + str(track) + ' from cache')
-        cache.pop(track)
-
-    write_cache(cache)
-    return cache
-
-def clear_cache():
-    write_cache({})
-
-def get_track_lyrics(track):
+def lyrics_for_track(track):
     lyrics = ''
     try:
         lyrics = PyLyrics.getLyrics(
@@ -71,18 +24,10 @@ def get_track_lyrics(track):
         lyrics = lyrics.split('...')[0] # remove text watermark
     return lyrics
 
-def get_top_40():
-    info_list = musixmatch.chart_tracks_get(1, 40, True) \
-            ['message']['body']['track_list']
-    return [Track(info) for info in info_list]
-
-def make_tweet():
-    cache = read_cache()
-
-    if not cache:
-        cache = update_cache()
- 
-    all_lyrics = ''.join(cache.values())
+def make_lyric():
+    all_lyrics = ''
+    for track in top_40_tracks():
+        all_lyrics += lyrics_for_track(track)
     models = [
         markovify.NewlineText(all_lyrics, state_size = 2),
         markovify.NewlineText(all_lyrics, state_size = 3),
