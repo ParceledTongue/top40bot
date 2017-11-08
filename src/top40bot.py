47e06f2e1db7f4cc3f61145e74ec242c88c22434
Zach Palumbo <zach@palumbo.io>
2017-11-07 20:50:50 -0500

    Post every 30 min instead of every 5 min
    Signed-off-by: Zach Palumbo <zach@palumbo.io>

--- src/top40bot.py
+++ src/top40bot.py
@@ -6,7 +6,7 @@ from history import History
 from musixmatch import Musixmatch
 
 HISTORY_SIZE = 1500
-TWEET_PERIOD = dt.timedelta(minutes = 5)
+TWEET_PERIOD = dt.timedelta(minutes = 30)
 
 # login via tweepy
 print('Logging in via tweepy')
